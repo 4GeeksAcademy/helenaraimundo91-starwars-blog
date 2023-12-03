28431219e13754397b55f1675e79112817aea706
helenaraimundo91 <139161478+helenaraimundo91@users.noreply.github.com>
2023-12-03 19:27:00 +0000

    project started

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,38 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			characters: [],
+			planets: [],
+			starships: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
+			getPeople: async () => {
 				const store = getStore();
+				const textResponse = await fetch("https://swapi.dev/api/people");
+				const jsonResponse = await textResponse.json();
+				setStore ({...store, people: jsonResponse.resolts});
+			};
+			getPlanet: async () => {
+				const store = getStore();
+				await fetch("https://swapi.dev/api/planets")
+				const jsonResponse = await textResponse.json();
+				setStore ({...store, planets: jsonResponse.resolts});
+			};
+			getStarship: async () => {
+				const store = getStore();
+				await fetch("https://swapi.dev/api/starships")
+				const jsonResponse = await textResponse.json();
+				setStore ({...store, starships: jsonResponse.resolts});
+			};
+			
+			getPerson: () => {
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+			}
+			getPlanet: () => {
+				
+			}
+			getStarship: () => {
+				
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,38 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useEffect } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const { store, actions } = useContext(Context)
+
+	const 
+	useEffect (() => {
+		actions.getPeople();
+		actions.getPlanets();
+		actions.getStarships();
+	}, [])
+
+	useEffect(() => {
+		handleInitialFetch();
+	}, []);
+		
+		return (
+		<div className="text-center mt-5">
+			<h1>Hello Starwars!</h1>
+			<div>
+				<h2>People:</h2>
+				{store.people.map((person) => { return <p key={person.uid}>{person.name}</p> })}
+			</div>
+			<div>
+				<h2>Planets:</h2>
+				{store.planets.map((planet) => { return <p key={planet.uid}>{planet.name}</p> })}
+			</div>
+			<div>
+				<h2>Starships:</h2>
+				{store.starskip.map((starship) => { return <p key={starship.uid}>{starship.name}</p> })}
+			</div>
+		</div>
+	);
+};
+
+
